53bc9ebcaf33cdcd7722a7c1a3be03d4fe851e8c
mqxu <moqi1977@gmail.com>
2023-04-19 16:10:47 +0800

    项目初始化

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+		curly: 'error', // 控制语句需要大括号
+		'vue/multi-word-component-names': 'off'  // 组件名不需要多个单词
+	}
+}
