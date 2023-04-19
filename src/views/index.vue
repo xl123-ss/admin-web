e38196bdab86cbde2076c7447bbbeb5505621c09
mqxu <moqi1977@gmail.com>
2023-04-19 16:12:48 +0800

    引入Element Plus

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,3 +1,11 @@
 <template>
-	<h2>index</h2>
+	<el-row>
+		<el-button>Default</el-button>
+		<el-button type="primary">Primary</el-button>
+		<el-button type="success">Success</el-button>
+		<el-button type="info">Info</el-button>
+		<el-button type="warning">Warning</el-button>
+		<el-button type="danger">Danger</el-button>
+		<el-button>中文</el-button>
+	</el-row>
 </template>
